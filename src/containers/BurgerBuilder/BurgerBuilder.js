c2b47dfdc9e75dd99284c77b6bf9e03fb75b5621
ManjunathBPoojary <babumanjunath02@gmail.com>
2020-03-21 18:06:42 +0530

    burger-1.0

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Auxiliary from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component{
+    render(){
+        return (
+            <Auxiliary>
+                <Burger />
+                <div>Build Controls</div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default BurgerBuilder;
