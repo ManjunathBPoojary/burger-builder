d1029817b2a08d2d3471baf06cc637fa1b3e7bde
ManjunathBPoojary <babumanjunath02@gmail.com>
2020-03-22 17:12:35 +0530

    BuildControls are in progress!!

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,25 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    //To covert ingredient object passed  from BurgerBuilder to array of keys ["saled","cheese","meat"."bacon"]
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            })
+        }).reduce((arr, ele) => {
+            return arr.concat(ele)
+        }, []);
+
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
+
     return (
         <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="bread-bottom"/>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
         </div>
     );
 };
