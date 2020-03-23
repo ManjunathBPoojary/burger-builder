4c903e95b9d746805a08301c8afcf6d923482a82
ManjunathBPoojary <babumanjunath02@gmail.com>
2020-03-24 00:40:37 +0530

    Build Controls are in place with price.

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,10 +3,12 @@ import classes from './BuildControl.css';
 
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
-        <div>{props.label}</div>
-        <button>Less</button>
-        <button>More</button>
+        <div className={classes.Label}>{props.label}</div>
+        <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>Less</button>
+        <button className={classes.More} onClick={props.added}>More</button>
     </div>
 );
 
 export default buildControl;
+
+
