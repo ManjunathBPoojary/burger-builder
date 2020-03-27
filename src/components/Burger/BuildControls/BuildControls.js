8919cbbd7ee63e6959cc426728b05c72c5d94f89
ManjunathBPoojary <babumanjunath02@gmail.com>
2020-03-27 18:04:04 +0530

    Order button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -19,6 +19,9 @@ const buildControls = (props) => (
             removed={() =>props.ingredientsRemoved(ctrl.type)}
             disabled={props.disabled[ctrl.type]}/>
         ))}
+        <button 
+        className={classes.OrderButton}
+        disabled={!props.purchasable}>ORDER NOW</button>
     </div>
 );
 
