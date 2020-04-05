56aa4c4a1cdedcd9cc75d9ab28df7742742aa9ce
ManjunathBPoojary <babumanjunath02@gmail.com>
2020-04-05 19:49:09 +0530

    ajax with axios handson

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './NewPost.css';
 
 class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Manjunath'
+    }
+
+    postDataHandler = () => {
+        const data = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        };
+        axios.post('/posts', data)
+        .then(response => {
+            console.log(response);
+        });
     }
 
     render () {
@@ -19,10 +31,10 @@ class NewPost extends Component {
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
+                    <option value="Manjunath">Manjunath</option>
+                    <option value="Sharada">Sharada</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
+import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -9,10 +10,11 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
     componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('/posts')
         .then(response => {
             const posts = response.data.slice(0, 4);
             const updatedPost = posts.map(post => {
@@ -23,6 +25,9 @@ class Blog extends Component {
             })
             this.setState({posts: updatedPost});
             ///console.log(response);
+        }).catch(error =>{
+            //console.log(error);
+            this.setState({error: true});
         });
     }
 
@@ -31,13 +36,16 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post =>{
-            return <Post 
-            key={post.id} 
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}/>
-        });
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!!</p>
+        if(!this.state.error){
+            posts = this.state.posts.map(post =>{
+                return <Post 
+                key={post.id} 
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}/>
+            });
+        }
         return (
             <div>
                 <section className="Posts">
